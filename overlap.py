30a0125f7ca1b91a262886a7f4b21b6521c77447
wuzhigang05 <zhigangwu.bgi@gmail.com>
2014-05-01 16:26:19 -0700

    added support for filtration by depth

--- overlap.py
+++ overlap.py
@@ -12,6 +12,7 @@ matplotlib.use('PDF')
 
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn2, venn3
+import brewer2mpl as b2m
 
 def sensitivity(found, total):
     try:
@@ -51,7 +52,6 @@ def getSNPs(file):
 def calculate(files, P):
   snps = map(getSNPs, files)
   sensitivities = Series(map(lambda x: sensitivity(x, P), snps), index = files, name="TPR")
-  pdb.set_trace()
   specificities = Series(map(lambda x: fdr(x, P), snps), index = files, name="FDR")
   result = concat([sensitivities, specificities], axis=1)
   print result
@@ -90,24 +90,40 @@ def TPRandFDR(file, theoreticalSnps):
     print "#FP: ", len(FP)
     print "FDR: ", float(len(FP))/len(snps)
     
+def simpleaxis(ax):
+  ax.spines['top'].set_visible(False)
+  ax.spines['right'].set_visible(False)
+  ax.get_xaxis().tick_bottom()
+  ax.get_yaxis().tick_left()
+
 if __name__ == '__main__':
   o = sys.stdout
   e = sys.stderr
   theoreticalSnps = set(np.load("mutatedPos.npy") + 1)
   allSnps = theoreticalSnps 
   files = ["sam.raw.vcf", "gatk.raw.vcf", "varianttools.raw.vcf"]
-  results = []
   newFiles = []
   for f in files:
       tmp = [f]
       for i in range(1, 10):
         tmp.append(".".join(f.split('.')[:2] + [str(i), 'vcf']))
       newFiles += tmp 
-  # for f in newFiles[10:20]:
-  #     TPRandFDR(f, theoreticalSnps)
+
+  dpFiles = []
+  for f in files:
+    tmp = []
+    for i in range(3, 13):
+      tmp.append(".".join(f.split('.')[:2] + ['dp' + str(i), 'vcf']))
+    dpFiles += tmp
+
   for i in range(10):
       venn3Diagram(getSNPs(newFiles[i]), getSNPs(newFiles[i+10]), getSNPs(newFiles[i+20]), 
-              [newFiles[i], newFiles[i+10], newFiles[i+20]], "venn_%d.pdf" % i)
+              #[newFiles[i], newFiles[i+10], newFiles[i+20]], "venn_%d.pdf" % i)
+              ["Samtools", "GATK", "VariantTools"], "venn_%d.pdf" % i)
+      
+      venn3Diagram(getSNPs(dpFiles[i]), getSNPs(dpFiles[i+10]), getSNPs(dpFiles[i+20]), 
+              ["Samtools", "GATK", "VariantTools"], "venn_dp%d.pdf" % i)
+
   sams = calculate(newFiles[:10], allSnps)
   gatks = calculate(newFiles[10:20], allSnps)
   varianttools = calculate(newFiles[20:], allSnps)
@@ -116,11 +132,31 @@ if __name__ == '__main__':
   # ax.plot(sams['FDR'], sams['TPR'], 'r-^', label='samtools/bcftools')
   # ax.plot(gatks['FDR'], gatks['TPR'], 'b-o', label='gatkUG')
   # ax.plot(varianttools['FDR'], varianttools['TPR'], 'g-s', label='Varianttools')
-  ax.plot(sams['TPR'], sams['FDR'], 'r-^', label='samtools/bcftools')
-  ax.plot(gatks['TPR'], gatks['FDR'], 'b-o', label='gatkUG')
-  ax.plot(varianttools['TPR'], varianttools['FDR'], 'g-s', label='Varianttools')
+
+  #colors = b2m.get_map('RdYlGn', 'Diverging', 3).mpl_colors
+  colors = ["red", "green", "blue"]
+  ax.plot(sams['TPR'], sams['FDR'], 'r-^', label='Samtools', color=colors[0])
+  ax.plot(gatks['TPR'], gatks['FDR'], 'b-o', label='GATKUG', color=colors[1])
+  ax.plot(varianttools['TPR'], varianttools['FDR'], 'g-s', label='Varianttools', color=colors[2])
+
   ax.set_ylabel("False Discovery Rate")
   ax.set_xlabel("True Positive Rate")
-  ax.legend(loc=0)
+  ax.set_title("Filtration by QUAL")
+  ax.legend(loc=0, frameon=False, fontsize='small')
+  simpleaxis(ax)
   fig.savefig("plot.pdf")
+
+  sams = calculate(dpFiles[:10], allSnps)
+  gatks = calculate(dpFiles[10:20], allSnps)
+  varianttools = calculate(dpFiles[20:], allSnps)
   
+  fig, ax = plt.subplots()
+  ax.plot(sams['TPR'], sams['FDR'], 'r-^', label='Samtools', color=colors[0])
+  ax.plot(gatks['TPR'], gatks['FDR'], 'b-o', label='GATK', color=colors[1])
+  ax.plot(varianttools['TPR'], varianttools['FDR'], 'g-s', label='VariantTools', color=colors[2])
+  ax.set_ylabel("False Discovery Rate")
+  ax.set_xlabel("True Positive Rate")
+  ax.set_title("Filtration by Read Coverage")
+  ax.legend(loc=0, frameon=False, fontsize='small')
+  simpleaxis(ax)
+  fig.savefig("tprDP.pdf")
